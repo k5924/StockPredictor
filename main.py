2529939519343c409f5526c88a256265fa30c426
Kamran <56407135+k5924@users.noreply.github.com>
2021-09-14 23:03:20 +0100

    SECTION: loaded test data

--- main.py
+++ main.py
@@ -55,3 +55,19 @@ model.add(Dense(units=1))  # prediction of the next closing price
 
 model.compile(optimizer='adam', loss='mean_squared_error')
 model.fit(x_train, y_train, epochs=25, batch_size=32)
+
+# Test the models accuracy on existing data
+
+# Load test data
+test_start = dt.datetime(2020, 1, 1)
+test_end = dt.datetime.now()
+
+test_data = web.DataReader(tickerSymbol, financeAPI, test_start, test_end)
+actual_prices = test_data['Close'].values
+
+total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
+
+model_inputs = total_dataset[len(
+    total_dataset) - len(test_data) - prediction_days:].values
+model_inputs = model_inputs.reshape(-1, 1)
+model_inputs = scalar.transform(model_inputs)

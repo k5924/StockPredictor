8fdf7bae82e2df0bd762f8537d910121416f84d5
Kamran <56407135+k5924@users.noreply.github.com>
2021-09-14 22:49:36 +0100

    SECTION: prepare data

--- main.py
+++ main.py
@@ -18,3 +18,21 @@ endDate = dt.datetime(2020, 1, 1)
 
 data = web.DataReader(tickerSymbol, financeAPI, startDate, endDate)
 
+# Prepare Data
+
+scalar = MinMaxScalar(feature_range=(0, 1))
+scaled_data = scalar.fit_transform(data['Close'].values.reshape(-1, 1))
+
+# days in the past to base prediction on
+prediction_days = 1825
+
+x_train = []
+y_train = []
+
+for x in range(prediction_days, len(scaled_data)):
+    x_train.append(scaled_data[x - prediction_days: x, 0])
+    y_train.append(scaled_data[x, 0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+
+x_train = np.array(x_train, (x_train.shape[0], x_train.shape[1], 1))

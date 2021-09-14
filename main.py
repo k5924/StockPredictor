519f09796f8bc572cc14602e33de27e9f1bacfda
Kamran <56407135+k5924@users.noreply.github.com>
2021-09-14 23:25:16 +0100

    SECTION: plotted test data

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 import pandas_datareader as web
 import datetime as dt
 
-from sklearn.preprocessing import MinMaxScalar
+from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM
 
@@ -20,7 +20,7 @@ data = web.DataReader(tickerSymbol, financeAPI, startDate, endDate)
 
 # Prepare Data
 
-scalar = MinMaxScalar(feature_range=(0, 1))
+scalar = MinMaxScaler(feature_range=(0, 1))
 scaled_data = scalar.fit_transform(data['Close'].values.reshape(-1, 1))
 
 # days in the past to base prediction on
@@ -35,7 +35,7 @@ for x in range(prediction_days, len(scaled_data)):
 
 x_train, y_train = np.array(x_train), np.array(y_train)
 
-x_train = np.array(x_train, (x_train.shape[0], x_train.shape[1], 1))
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
 # Build the Model
 
@@ -83,3 +83,13 @@ x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
 predicted_prices = model.predict(x_test)
 predicted_prices = scalar.inverse_transform(predicted_prices)
+
+# Plot test data predictions
+plt.plot(actual_prices, color="black", label=f"Actual {tickerSymbol} Price")
+plt.plot(predicted_prices, color="green",
+         label=f"Predicted {tickerSymbol} Price")
+plt.title(f"{tickerSymbol} Share Price")
+plt.xlabel('Time')
+plt.ylabel(f"{tickerSymbol} Share Price")
+plt.legend()
+plt.show()
